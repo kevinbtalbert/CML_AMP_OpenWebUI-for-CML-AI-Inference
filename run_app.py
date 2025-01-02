8f53eb5fc671b90c454e9a1044950a134b616359
Kevin Talbert <ktalbert@cloudera.com>
2025-01-02 11:54:08 -0500

    updated commit

--- run_app.py
+++ run_app.py
@@ -1,7 +1,9 @@
 # Fix needed, see https://github.com/open-webui/open-webui/discussions/5190#discussioncomment-10963146
-!mkdir .local/lib/python3.11/site-packages/google/colab
+# Create necessary directory
+!mkdir -p .local/lib/python3.11/site-packages/google/colab
 
-!OPENAI_API_BASE_URLS="https://go01-aws-ml-serving.go01-dem.ylcu-atmi.cloudera.site/namespaces/serving-default/endpoints/llama-3-1-8b-instruct/v1" \
- OPENAI_API_KEYS="YOURSHERE" \
+# Run the web UI with environment variables populated
+!OPENAI_API_BASE_URLS=$INFERENCE_SERVICE_BASE_URL \
+ OPENAI_API_KEYS=$(grep -o '"access_token":"[^"]*' "/tmp/jwt" | sed 's/"access_token":"//') \
  WEBUI_AUTH=False \
  open-webui serve --host 127.0.0.1 --port $CDSW_APP_PORT
